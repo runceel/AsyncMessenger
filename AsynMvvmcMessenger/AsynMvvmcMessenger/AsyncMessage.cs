7cff927d273f180af2415d65ce48ab7c0684decd
okazuki <k.ota.0130@gmail.com>
2014-05-13 20:24:45 +0900

    create infrastructure

---
+++ AsynMvvmcMessenger/AsynMvvmcMessenger/AsyncMessage.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynMvvmcMessenger
+{
+    class AsyncMessage<TMessage> : MessageBase
+        where TMessage : MessageBase
+    {
+        private readonly TaskCompletionSource<object> source = new TaskCompletionSource<object>();
+        public TMessage InnerMessage { get; set; }
+
+        public Task<object> Task { get { return this.source.Task; } }
+
+        public void SetResult(object result)
+        {
+            this.source.SetResult(result);
+        }
+
+        public void SetException(Exception ex)
+        {
+            this.source.SetException(ex);
+        }
+    }
+}
---
+++ AsynMvvmcMessenger/AsynMvvmcMessenger/AsyncMessageReceiver.cs
@@ -0,0 +1,58 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsynMvvmcMessenger
+{
+    class AsyncMessageReceiver<TMessage> : IDisposable
+        where TMessage : MessageBase
+    {
+        private IMessenger messenger;
+        private Func<TMessage, Task<object>> callback;
+        private object token;
+
+        public AsyncMessageReceiver(IMessenger messenger, 
+            object token,
+            bool receiveDerivedMessagesToo,
+            Func<TMessage, Task<object>> callback)
+        {
+            this.messenger = messenger;
+            this.token = token;
+            this.callback = callback;
+            messenger.Register<AsyncMessage<TMessage>>(
+                this,
+                token,
+                receiveDerivedMessagesToo,
+                this.ReceiveAsyncMessage)
+;
+        }
+
+        private async void ReceiveAsyncMessage(AsyncMessage<TMessage> m)
+        {
+            try
+            {
+                var result = await this.callback(m.InnerMessage);
+                m.SetResult(result);
+            }
+            catch (Exception ex)
+            {
+                m.SetException(ex);
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.callback == null)
+            {
+                return;
+            }
+            this.messenger.Unregister<AsyncMessage<TMessage>>(this, this.token, this.ReceiveAsyncMessage);
+            this.callback = null;
+            this.token = null;
+            this.messenger = null;
+        }
+    }
+}
